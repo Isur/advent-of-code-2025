8a8c8f0adb316c7fe444267128e493fd15d3911e
Isur <bednarczykartur96@gmail.com>
2025-12-09 19:10:00 +0100

    feat: Day09 part 1

--- days/Day09/run.go
+++ days/Day09/run.go
@@ -2,6 +2,7 @@ package Day09
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/Isur/advent-of-code-2025/pkg"
@@ -12,14 +13,53 @@ func Run(data []string) {
 	fmt.Println("Part 2:", part2(data))
 }
 
+type Box struct {
+	X int
+	Y int
+}
+
+func CreateBoxFromLine(line string) Box {
+	coords := strings.Split(line, ",")
+	x := pkg.ParseToInt(coords[0])
+	y := pkg.ParseToInt(coords[1])
+	box := Box{
+		X: x,
+		Y: y,
+	}
+
+	return box
+}
+
+func CalcBoxSize(a, b Box) int {
+	one := pkg.Abs(a.X, b.X) + 1
+	two := pkg.Abs(a.Y, b.Y) + 1
+
+	size := one * two
+
+	return size
+}
+
 func part1(data []string) int {
 	start := time.Now()
 	defer func() {
 		elapsed := time.Since(start)
 		pkg.PrintTiming(elapsed, 1)
 	}()
-	// TODO: Implement part 1
-	return 0
+	boxes := []Box{}
+	biggestSize := -1
+
+	for _, line := range data {
+		box := CreateBoxFromLine(line)
+		for _, b := range boxes {
+			size := CalcBoxSize(box, b)
+			if size > biggestSize {
+				biggestSize = size
+			}
+		}
+		boxes = append(boxes, box)
+	}
+
+	return biggestSize
 }
 
 func part2(data []string) int {
@@ -28,6 +68,6 @@ func part2(data []string) int {
 		elapsed := time.Since(start)
 		pkg.PrintTiming(elapsed, 2)
 	}()
-	// TODO: Implement part 2
+
 	return 0
 }
